526b5e29c45908cd68ae6bff8e920a4ae2a85e6d
shooooooma415 <shoma20040415@icloud.com>
2025-02-21 04:16:46 +0900

    feat:textAreaを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+import { Textarea } from "@heroui/input";
+import { useState } from "react";
+import { Alert } from "@heroui/alert";
+
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
+  const [inputValue, setInputValue] = useState("初期だよ");
+
+  const handleChangeValue = (inputValue: string) => {
+    setInputValue(inputValue);
+  };
+
   return (
     <DefaultLayout>
-      <div>kontiha</div>
+      <Textarea
+        className="w-full"
+        label="Description"
+        placeholder="Enter your description"
+        onValueChange={handleChangeValue}
+      />
+      <Alert color={"success"} title={inputValue} />
     </DefaultLayout>
   );
 }
